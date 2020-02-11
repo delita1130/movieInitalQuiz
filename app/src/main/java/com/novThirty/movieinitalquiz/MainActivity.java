ac13330ea6a6e6a7f79a74c09d1ae96871728d36
김윤용 <kyy3326@gmail.com>
2020-02-11 18:31:40 +0900

    충돌테스트

--- app/src/main/java/com/novThirty/movieinitalquiz/MainActivity.java
+++ app/src/main/java/com/novThirty/movieinitalquiz/MainActivity.java
@@ -2,15 +2,47 @@ package com.novThirty.movieinitalquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    DataBaseHelper dbHelper;
+    SQLiteDatabase database;
+    String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//,,,
-        //
-        //
+        Log.e(TAG, "시작");
+        setContentView(R.layout.activity_main);
+        createDatabase();
+        insertRecord();
+    }
+
+    private void createDatabase() {
+        dbHelper = new DataBaseHelper(this);
+
+        dbHelper = new DataBaseHelper(this);
+        database = dbHelper.getWritableDatabase();
+
+
+    }
+
+
+    public void insertRecord() {
+        if (database != null) {
+            database.execSQL("insert into xb_movie(" +
+                    "mov_name," +
+                    "stage," +
+                    "step," +
+                    "mov_script," +
+                    "mov_actor," +
+                    "mov_img_path"
+                    + ") values ('영화 제목', '1단계', '1', '안녕하세요', '홍길동, 외 1명', 'aaa')");
+        }
     }
 }
---
+++ app/src/main/java/com/novThirty/movieinitalquiz/model/Movie.java
@@ -0,0 +1,11 @@
+package com.novThirty.movieinitalquiz.model;
+
+public class Movie {
+    private int movNum;
+    private String movName;
+    private String stage;
+    private String step;
+    private String movScript;
+    private String movActor;
+    private String movImgPath;
+}
