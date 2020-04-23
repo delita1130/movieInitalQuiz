bf8e7a94528e48a228987f790868418eede11ff0
김윤용 <kyy3326@gmail.com>
2020-04-23 16:49:15 +0900

    스테이지 버그 개선중

--- app/src/main/java/com/novThirty/movieinitalquiz/adapter/StageAdapter.java
+++ app/src/main/java/com/novThirty/movieinitalquiz/adapter/StageAdapter.java
@@ -50,22 +50,30 @@ public class StageAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(final int i, View view, ViewGroup viewGroup) {
-        view = mInflater.inflate(R.layout.item_stage, viewGroup, false);
+    public View getView(final int i, View convertView, ViewGroup viewGroup) {
+        View view = convertView;
+        if(view == null){
+            view = mInflater.inflate(R.layout.item_stage, viewGroup, false);
+        }
 
         ImageButton stageBtn = view.findViewById(R.id.stageBtn);
         stageBtn.setLayoutParams(new LinearLayout.LayoutParams(450, 500));
 
         User user = GameStatus.user;
 
-        final boolean flag = user.getCurrMovNum() >= items.get(i).getMovNum();
+        // 완료 번호가 문제의 번호보다 크거나 같으면 stage을 열어 둔다.
+        // 첫번째 stage는 무조건 연다. 처음 시작하면 완료번호가 0이기 때문에 열어 둔다.
 
-        Log.d("test :: ", "");
+        final boolean flag = user.getDoneMovNum() + GameStatus.numOfstepPerStage + 1 >= items.get(i).getMovNum();
 
-        // 최초 user의 currMovNum은 1 이여야 함.
-        if(flag){
+        Log.d("test :: 루프 번호 : ", i + "");
+        Log.d("test :: 문제 번호 : ", items.get(i).getMovNum() + "");
+        Log.d("test :: 완료 번호 : ", user.getDoneMovNum() + 1 + "");
+        Log.d("test :: 스테이지 번호 : ", items.get(i).getStage() + "");
+
+        if(flag) {
             stageBtn.setImageResource(R.drawable.ic_open_stage);
-        }else{
+        }else {
             stageBtn.setImageResource(R.drawable.ic_lock_stage);
         }
 
