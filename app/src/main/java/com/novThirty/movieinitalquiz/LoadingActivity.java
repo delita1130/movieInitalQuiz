24aedebd3c66f04aa2494793c7b78ef1af49fe69
김윤용 <kyy3326@gmail.com>
2020-06-15 16:18:13 +0900

    스플래시 제 적용

--- app/src/main/java/com/novThirty/movieinitalquiz/LoadingActivity.java
+++ app/src/main/java/com/novThirty/movieinitalquiz/LoadingActivity.java
@@ -1,5 +1,6 @@
 package com.novThirty.movieinitalquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -11,16 +12,21 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
-        startLoading();
+
+        Handler hd = new Handler();
+        hd.postDelayed(new splashHandler(), 2000);
 
     }
-    private void startLoading() {
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                finish();
-            }
-        }, 2000);
+
+    private class splashHandler implements Runnable{
+        public void run(){
+            startActivity(new Intent(getApplication(), MainActivity.class));
+            LoadingActivity.this.finish();
+        }
+    }
+
+    @Override
+    public void onBackPressed(){
+        // 스플래시 화면에서 뒤로가기 버튼 못하게 만듦..
     }
 }
--- app/src/main/java/com/novThirty/movieinitalquiz/MainActivity.java
+++ app/src/main/java/com/novThirty/movieinitalquiz/MainActivity.java
@@ -38,8 +38,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_stage);
-        Intent intent = new Intent(this, LoadingActivity.class);
-        startActivity(intent);
 
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
