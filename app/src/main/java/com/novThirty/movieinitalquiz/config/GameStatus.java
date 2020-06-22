32309deca97030416a47a88af3571d78a28fa0db
delita <delita1130@gmail.com>
2020-06-22 16:30:43 +0900

    UI 다시다시다시 수정본

--- app/src/main/java/com/novThirty/movieinitalquiz/config/GameStatus.java
+++ app/src/main/java/com/novThirty/movieinitalquiz/config/GameStatus.java
@@ -6,11 +6,11 @@ public class GameStatus {
     public static User user;
     public static int numOfstepPerStage = 5; // 스테이지 당 스템의 개수
 
-    public static int movScriptHintPoint = 10;
-    public static int movActorHintPoint = 15;
-    public static int movImgHintPoint = 20;
-    public static int movNameHintPoint = 50;
+    public static int movScriptHintPoint = 250;
+    public static int movActorHintPoint = 250;
+    public static int movImgHintPoint = 250;
+    public static int movNameHintPoint = 500;
 
-    public static int movAnswerPoint = 100;
-    public static int movRewardPoint = 1000;
+    public static int movAnswerPoint = 10;
+    public static int movRewardPoint = 1500;
 }
